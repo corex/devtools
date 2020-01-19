81b371c706132454eb8b40e139554999330383b4
CoRex <dev@corex.dk>
2020-01-19 15:37:08 +0100

    Updated CodeSniffer definitions and tools.

--- phpcs-loose.sh
+++ phpcs-loose.sh
@@ -3,16 +3,16 @@
 CURDIR=$(pwd)
 SYSDIR=$(dirname $(readlink -f $0))
 
-if [ ! -f "$SYSDIR/vendor/bin/phpcs" ]; then
+if [[ ! -f "$SYSDIR/vendor/bin/phpcs" ]]; then
 	echo "Could not find phpcs"
 	echo ""
 	echo "You need to run ./setup.sh"
 	exit
 fi
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
 	echo "You need to specify at least 1 argument."
 	exit
 fi
 
-$SYSDIR/vendor/bin/phpcs --standard=$SYSDIR/CodeSniffer/ruleset-strict.xml $1 $2 $3 $4 $5 $6 $7 $8 $9
+$SYSDIR/vendor/bin/phpcs --standard=$SYSDIR/CodeSniffer/ruleset-loose.xml $1 $2 $3 $4 $5 $6 $7 $8 $9
--- setup.sh
+++ setup.sh
@@ -4,8 +4,14 @@ CURDIR=$(pwd)
 SYSDIR=$(dirname $(readlink -f $0))
 cd $SYSDIR
 
+# Check if php is installed on system.
+if [[ ! -f "/usr/bin/php" ]]; then
+    echo "PHP /usr/bin/php not installed on system."
+    exit
+fi
+
 # Make sure composer is installed.
-if [ ! -f "$SYSDIR/composer.phar" ]; then
+if [[ ! -f "$SYSDIR/composer.phar" ]]; then
 	echo "Download composer."
 	echo ""
 
@@ -13,7 +19,7 @@ if [ ! -f "$SYSDIR/composer.phar" ]; then
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-	if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+	if [[ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]]
 	then
 	    >&2 echo 'ERROR: Invalid installer signature'
 	    rm composer-setup.php
@@ -30,27 +36,53 @@ echo ""
 php composer.phar selfupdate
 echo ""
 
-# Updating FXP asset plugin.
-php composer.phar global update fxp/composer-asset-plugin --no-plugins
+# Updating FXP asset plugin.1
+if [[ -f "$HOME/.composer/vendor/fxp/composer-asset-plugin" ]]; then
+    php composer.phar global update fxp/composer-asset-plugin --no-plugins
+else
+    php composer.phar global require fxp/composer-asset-plugin
+fi
 echo ""
+
+# Link composer.phar to composer
+#if [[ ! -f "$SYSDIR/composer" ]]; then
+#    ln -s $SYSDIR/composer.phar $SYSDIR/composer
+#fi
 
 # Install/Update composer.json
 php composer.phar update
 echo ""
 
-# Link composer.phar to composer
-if [ ! -f "$SYSDIR/composer" ]; then
-    ln -s $SYSDIR/composer.phar $SYSDIR/composer
-fi
-echo ""
-
 # Setup PHP CodeSniffer
-echo "Setting up Code Sniffer."
-$SYSDIR/vendor/bin/phpcs --config-set colors 1
-$SYSDIR/vendor/bin/phpcs --config-set show_progress 1
-$SYSDIR/vendor/bin/phpcs --config-set report_width 120
-$SYSDIR/vendor/bin/phpcs --config-set encoding utf-8
-echo ""
+#echo "Setting up Code Sniffer."
+#$SYSDIR/vendor/bin/phpcs --config-set show_progress 1
+#$SYSDIR/vendor/bin/phpcs --config-set encoding utf-8
+#echo ""
+
+# If ~/bin exists, add symbolic links.
+if [[ -d "$HOME/bin" ]]; then
+
+    # Add link to composer.phar in ~/bin
+    if [[ ! -f "$HOME/bin/composer" ]]; then
+        ln -s $SYSDIR/composer $HOME/bin/composer
+        echo "Link $HOME/bin/composer created."
+        echo ""
+    fi
+
+    # Add link to phpcs-loose in ~/bin
+    if [[ ! -f "$HOME/bin/phpcs-loose" ]]; then
+        ln -s $SYSDIR/phpcs-loose.sh $HOME/bin/phpcs-loose
+        echo "Link $HOME/bin/phpcs-loose created."
+        echo ""
+    fi
+
+    # Add link to phpcs-strict in ~/bin
+    if [[ ! -f "$HOME/bin/phpcs-strict" ]]; then
+        ln -s $SYSDIR/phpcs-strict.sh $HOME/bin/phpcs-strict
+        echo "Link $HOME/bin/phpcs-strict created."
+        echo ""
+    fi
+fi
 
 cd $CURDIR
 echo "Setup done."
